1778a31b0a1cdf1e6e2a8e06da6da6ee2be9148d
David Chen <chen7david@me.com>
2020-12-05 06:53:42 +0800

    update

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const p = require('path')
-const dirpath = p.resolve(__dirname, './../', 'config')
+const dirpath = p.resolve(__dirname, './../../', 'config')
 
 function getConfig(path){
     const yaml = require('js-yaml')
     const fs   = require('fs')
-    const p   = require('path')
-    const path = p.resolve(__dirname,'config')
+    console.log({path})
     const files = fs.readdirSync(path)
     const config = {}
     files.forEach(file => {
